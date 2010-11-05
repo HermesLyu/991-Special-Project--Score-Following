a46a400fd20c166c1c6fafd26df9018c2be0b4cd
Hermes Lyu <a108210@gmail.com>
2010-11-05 22:02:25 +0800

    add findpath.m - a recursive code to find path of match, sitll with x-y inverted not knowing why

--- alig.m
+++ alig.m
@@ -1,22 +1,26 @@
-resolution = 0.25;
+resolution = 0.25; %unit of each pixel in the map. should multiple of 5
 map  = zeros((length(chnumsavg)*timeratemid/resolution), (length(env)*timeratewav/resolution)); %map with midi at y and wav at x,
-for i = 1:length(transwav)
+for x = 1:length(transwav)
     %index = size(map, 1)-round(transwav(1, i)/resolution); %round to int, start from lower left corner
-    index = round(transwav(1, i)/resolution);
-    if transwav(2, i) > 0
+    index = round(transwav(1, x)/resolution);
+    if transwav(2, x) > 0
         map(:, index) = map(:, index)+1; %up
-    elseif transwav(2, i) < 0
+    elseif transwav(2, x) < 0
         map(:, index) = map(:, index)-1; %down
     end
 end
-for j = 1:length(transmid)
-    index = round(transmid(1, j)/resolution);
-    if transmid(2, j) > 0
+for y = 1:length(transmid)
+    index = round(transmid(1, y)/resolution);
+    if transmid(2, y) > 0
         map(index, :) = map(index, :)+1;
-    elseif transmid(2, j) < 0
+    elseif transmid(2, y) < 0
         map(index, :) = map(index, :)-1;
     end
 end
+aligment = [];
+aligment = findpath(1, 1, map, aligment);
+aligment = [[0; 0], aligment];
+aligment = aligment.*resolution;
 map1 = (map+2)/4; %shift and normalize for poltting.
 
 %plot
@@ -26,14 +30,18 @@ hold on;
 axis on;
 ylabel('midi')
 xlabel('wav')
-scatter(transwav(1, :), transmid(1, :), 'r')
+%scatter(transwav(1, :), transmid(1, :), 'r')
 plot([0, size(map1, 2)], [0, size(map1, 1)])
 
-[row, col] = find(map1 == 1); %mark 1's
-scatter(col*resolution, row*resolution, 'b')
+% [row, col] = find(map1 == 1); %mark 1's
+% scatter(col*resolution, row*resolution, 'b')
+% 
+% [row, col] = find(map1 == 0); %mark 0's
+% scatter(col*resolution, row*resolution, 'g')
 
-[row, col] = find(map1 == 0); %mark 0's
-scatter(col*resolution, row*resolution, 'g')
+%scatter(aligment(2, :), aligment(1, :), 'r')
+plot(aligment(1, :), aligment(2, :), 'ro-')
+
 hold off;
 % p = 0;
 % q = 0;
