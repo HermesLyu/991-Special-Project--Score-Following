3bbf06ad841d26250acd8035795e073d30442c90
Hermes Lyu <a108210@gmail.com>
2010-11-11 19:58:45 +0800

    DTW needs test

--- DTWcompute.m
+++ DTWcompute.m
@@ -4,29 +4,39 @@
     %for 
         DTW(:, 1) = Inf;
     DTW(1, 1) = 0;
-path = [1;1];
+map= zeros(size(DTW, 1), size(DTW, 2), 2);
 
     for x = 2:length(transwav)+1; %start from 2,2 because DTW starts with 1, 1
         for y = 2:length(transmid)+1;
             cost = abs(transwav(2, x-1) - transmid(2, y-1));
-            DTW(y, x) = cost + min([DTW(y-1, x  ),    % insertion
-                                        DTW(y  , x-1),    % deletion
+            DTW(y, x) = cost + min([DTW(y-1, x  )*1.4,    % insertion
+                                        DTW(y  , x-1)*1.4,    % deletion
                                         DTW(y-1, x-1)]) ;   % match
             [C, I] = min([DTW(y-1, x  ),DTW(y  , x-1),DTW(y-1, x-1)]);
             if I == 1 
-                path = [path,[y-1; x]];
+                map(y, x, 1) = y-1;
+                map(y, x, 2) = x;
             elseif I == 2
-                path = [path,[y; x-1]];
+                map(y, x, 1) = y;
+                map(y, x, 2) = x-1;
             elseif I == 3
-                path = [path, [y-1; x-1]];
+                map(y, x, 1) = y-1;
+                map(y, x, 2) = x-1;
+                
             end
    
         end
     end
-    path = path-1;
-    DTW = DTW(2:end, 2:end)
+    map = map-1;
+    map = map(2:end, 2:end, :);
+    DTW = DTW(2:end, 2:end);
     DTW1 = DTW/max(max(DTW));
     imshow(DTW1, 'InitialMagnification', 'fit')
     set(gca,'YDir','normal')
+    xlabel('wav')
+    ylabel('mid')
+    hold on;
     
+    bestpath = DTWfindpath(map, [size(map, 1); size(map, 2)]);
+    plot(bestpath(2, :), bestpath(1, :), 'ro-')
     
--- aligout.m
+++ aligout.m
@@ -3,3 +3,8 @@ hold
 bar(transwav(1, :), transwav(2, :)/max(abs(transwav(2, :))), 'r')
 legend('mid', 'wav')
 grid on
+figure(2)
+subplot(2, 1, 1)
+runmidi
+subplot(2, 1, 2)
+runwav
