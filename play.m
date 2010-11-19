7d9de44a92b180951c92283d42994007ce5a47b2
Hermes Lyu <a108210@gmail.com>
2010-11-19 16:24:07 +0800

    play updated

--- play.m
+++ play.m
@@ -1,7 +1,29 @@
 t = 0;
+tmid = 0;
+step = 1;
+seg = 1; %segment number = index number of path and speed
+speedtemp = path(:, 1:end-1)-path(:, 2:end); %diff
+speed = speedtemp(1, :)/speedtemp(2, :); %find the speed (mid/wav = tmid/t) for each segment
 pianoroll(mid, 'sec')
+
+%wavplay(wav, fs) %play the wave
+t = timer('Period', step, TimerFcn',
 while t < max(mid(:, 6))
-axis([t t+20 30 95])
-t = t+0.1;
-pause(0.1); %pause for 0.5 sec
+    if path(2, seg) <= t < path(2, seg+1)
+        axis([tmid-10 tmid+10 30 95])
+        %patch([tmid-10 tmid+10 tmid+10 tmid-10], [95 95 30 30], 'g') %draw the middle square
+        %rectangle('Position', [tmid-2 tmid+2 30 95])
+        tmid = tmid + step*speed(seg);
+        t = t + step;
+    else
+        seg = seg+1;
+        t = path(2, seg);
+        tmid = path(1, seg);
+        axis([tmid-10 tmid+10 30 95])
+        tmid = tmid + step*speed(seg);
+        t = t + step;
+    end
+pause(step); %pause for 0.5 sec
 end
+);
+start(t)
