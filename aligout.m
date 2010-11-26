c46f97faa104b0719db5309f0b4464ef0d33ff02
Hermes Lyu <a108210@gmail.com>
2010-11-26 18:12:15 +0800

    aligout modified

--- aligout.m
+++ aligout.m
@@ -3,15 +3,15 @@
 % bar(transwav(1, :), transwav(2, :)/max(abs(transwav(2, :))), 'r')
 % legend('mid', 'wav')
 % grid on
-figure(3)
-subplot(2, 1, 1)
+figure
+%subplot(2, 1, 1)
 runmidi
 ylabel('mid')
 grid on;
 grid minor
-subplot(2, 1, 2)
+%subplot(2, 1, 2)
+figure
 runwav
 ylabel('wav')
 grid on
 grid minor
-figure(1)
