ffea8249b9525066f9a2c73e732383118059ea92
Hermes Lyu <a108210@gmail.com>
2010-11-26 22:57:14 +0800

    minor change in alig.m

--- DTWfindpath.m
+++ DTWfindpath.m
@@ -1,6 +1,6 @@
 function bestpath = DTWfindpath(DTWmap, bestpath)
-x = DTWmap(bestpath(1),bestpath(2), 1)
-y = DTWmap(bestpath(1), bestpath(2), 2)
+x = DTWmap(bestpath(1),bestpath(2), 1);
+y = DTWmap(bestpath(1), bestpath(2), 2);
 
 if x <1 | y <1
     bestpath = [[0; 0],bestpath];
