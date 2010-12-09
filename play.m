16802ed673d4dd85e0b59a7040192afa02dd9fee
Hermes Lyu <a108210@gmail.com>
2010-12-09 17:16:09 +0800

    timer trying

--- play.m
+++ play.m
@@ -1,17 +1,26 @@
-t = 0;
-tmid = 0;
-step = 1;
-seg = 1; %segment number = index number of path and speed
-speedtemp = path(:, 1:end-1)-path(:, 2:end); %diff
-speed = speedtemp(1, :)/speedtemp(2, :); %find the speed (mid/wav = tmid/t) for each segment
-midlength = max(mid(:, 6));
+% t = 0;
+% tmid = 0;
+% step = 1;
+% seg = 1; %segment number = index number of path and speed
+% speedtemp = path(:, 1:end-1)-path(:, 2:end); %diff
+% speed = speedtemp(1, :)/speedtemp(2, :); %find the speed (mid/wav = tmid/t) for each segment
+% midlength = max(mid(:, 6));
 
 pianoroll(mid, 'sec')
 
 %wavplay(wav, fs) %play the wave
+speedtemp = path(:, 1:end-1)-path(:, 2:end); %diff
+speed = speedtemp(1, :)/speedtemp(2, :); %find the speed (mid/wav = tmid/t) for each segment
+midlength = max(mid(:, 6));
+
+
+ud = struct('t', 0, 'tmid', 0,  'step',1, 'seg', 1, 'speed', speed, 'midlength', midlength) ;
+
+
 
 t = 0;
-tim = timer('ExecutionMode','fixedRate','BusyMode','drop','Period',1, 'TimerFcn', 'showscore(t, tmid, step, seg, speed, midlength); tmid = tmid + step*speed(seg); t = t + step; seg=seg+1')
+tim = timer('ExecutionMode','fixedRate', 'BusyMode','drop','Period',1,'TimerFcn', 'showscore')
+set(tim,'UserData',ud);
 %set(tim,'ExecutionMode','fixedRate','BusyMode','drop','Period',1, 'TimerFcn', display(t); t = t+1);
 % while t < max(mid(:, 6))
 %     if path(2, seg) <= t < path(2, seg+1)
@@ -30,5 +39,11 @@ tim = timer('ExecutionMode','fixedRate','BusyMode','drop','Period',1, 'TimerFcn'
 %     end
 % %pause(step); %pause for 0.5 sec
 % end
+% t = 0;
+% tmid = 0;
+% step = 1;
+% seg = 1; %segment number = index number of path and speed
+
+
 
 start(tim);
--- showscore.m
+++ showscore.m
@@ -1,18 +1,27 @@
-function showscore(t, tmid, step, seg, speed, midlength)
+function showscore(obj, eventdata)
+ud=obj.UserData;
+    t = ud.t;
+    tmid = ud.tmid;
+    step = ud.step;
+    seg = ud.seg;
+    speed = ud.speed;
+    midlength = ud.midlength;
 while t < midlength
-    if path(2, seg) <= t < path(2, seg+1)
+    if path(2, seg) <= t < path(2, seg+1) %during const speed period
         axis([tmid-10 tmid+10 30 95])
         %patch([tmid-10 tmid+10 tmid+10 tmid-10], [95 95 30 30], 'g') %draw the middle square
         %rectangle('Position', [tmid-2 tmid+2 30 95])
-%         tmid = tmid + step*speed(seg);
-%         t = t + step;
+         ud.tmid = tmid + step*speed(seg);
+         ud.t = t + step;
+         set(obj,'UserData',ud);
     else
 %         seg = seg+1;
-        t = path(2, seg);
+        t = path(2, seg); %meet speed change point
         tmid = path(1, seg);
         axis([tmid-10 tmid+10 30 95])
-%         tmid = tmid + step*speed(seg);
-%         t = t + step;
+         ud.tmid = tmid + step*speed(seg);
+         ud.t = t + step;
+         set(obj,'UserData',ud);
     end
 %pause(step); %pause for 0.5 sec
 end
